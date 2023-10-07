d891b876968de894897b0b2d583868b8b053fae0
auto login <autologin@ib-410-03.ipa.psati.ru>
2023-10-07 17:15:53 +0400

    Реализовал интерфейс с кнопками для игры

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-import tkinter as tk
+from tkinter import *
+import tkinter as ttk
 import service
-print(service.random_word())
+
+#print(service.random_word())
+
+def show_message():
+
+    
+root = Tk()
+root.geometry('1000x500')
+root.title('Виселица')
+
+
+button1 = ttk.Button(root,text='Начать')
+button1.pack()
+button2 = ttk.Button(root,text='Жми', command = show_message)
+button2.pack
+
+
+entry = ttk.Entry()
+entry.pack(expand=TRUE)
+
+
+label = ttk.Label(root)
+label.place()
+label.pack()
+
+
+root.mainloop()
--- service.py
+++ service.py
@@ -1,9 +1,12 @@
 import random
-words = [
-    'Сердце',
-    'Вода',
-    'Защита'
-]
+
+
+sport = {
+    'Плавание': 'Вид спорта',
+    'Бассейн': 'Место, где люди тренируются в воде',
+    'Соревнования': 'Участники борятся между собой в погоне за победой',
+}
 def random_word():
-    random_q = random.choice(words)
+    random_q = random.choice(list(sport.items()))
     return random_q
+
